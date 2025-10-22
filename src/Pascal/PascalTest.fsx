e0dc5d3d3889a6994274c792f5794615c773810e
Ruben <magewiredev@gmail.com>
2025-10-22 21:36:51 +0200

    Add testing for non-recursive function

--- src/Pascal/PascalTest.fsx
+++ src/Pascal/PascalTest.fsx
@@ -11,16 +11,32 @@ let hasPropertyOne (n: int, k: int) =
         pascal (n,k) = 1
     else
         true
-
-let testPropertyOne = List.map (test hasPropertyOne)
-
+let hasPropertyOneNoRec (n: int, k: int) =
+    if n < 0 || k < 0 || k > n then
+        true
+    elif k = 0 || k = n then
+        pascalNoRec (n,k) = 1
+    else
+        true
 let hasPropertyTwo (n: int, k: int) =
     if k>0 && k < n then
         pascal (n, k) = pascal (n-1, k-1) + pascal (n-1, k)
     else
         true
 
+let hasPropertyTwoNoRec (n: int, k: int) =
+    if k>0 && k < n then
+        pascalNoRec (n, k) = pascalNoRec (n-1, k-1) + pascalNoRec (n-1, k)
+    else
+        true
+
+let testPropertyOne = List.map (test hasPropertyOne)
+
+let testPropertyOneNoRec = List.map (test hasPropertyOneNoRec)
+
 let testPropertyTwo = List.map (test hasPropertyTwo)
+
+let testPropertyTwoNoRec = List.map (test hasPropertyTwoNoRec)
 
 let testInputs = [
     (-2,-2); (0,-2); (1,-2); (5,-2);
@@ -29,18 +45,37 @@ let testInputs = [
     (-2,5); (0,5); (1,5); (5,5)
 ]
 
+
 let testPropertyOneResults () = testPropertyOne testInputs
+
+let testPropertyOneNoRecResults () = testPropertyOneNoRec testInputs
+
+let testPropertyTwoResults () = testPropertyTwo testInputs
+
+let testPropertyTwoNoRecResults () = testPropertyTwoNoRec testInputs
 
 let printTestResults results =
     results
     |> List.iter (function
-        | Ok (n,k) -> printfn "PASSED: (%d,%d)" n k
-        | Error (n,k) -> printfn "FAILED: (%d,%d)" n k
+        | Ok (n,k) -> printfn "PASSED: (%d,%d) -> %d" n k (pascal (n, k))
+        | Error (n,k) -> printfn "FAILED: (%d,%d) -> %d" n k (pascal (n, k))
     )
 
-let testPropertyTwoResults () = testPropertyTwo testInputs
+let printTestResultsNoRec results =
+    results
+    |> List.iter (function
+        | Ok (n,k) -> printfn "PASSED: (%d,%d) -> %d" n k (pascalNoRec (n, k))
+        | Error (n,k) -> printfn "FAILED: (%d,%d) -> %d" n k (pascalNoRec (n, k))
+    )
 
-printf "Property 1: "
+printf "RECURSION: \n"
+printf "Property 1: \n"
 testPropertyOneResults () |> printTestResults
 printf "Property 2: "
 testPropertyTwoResults () |> printTestResults
+
+printf "NO RECURSION: \n"
+printf "Property 1: \n"
+testPropertyOneNoRecResults () |> printTestResultsNoRec
+printf "Property 2: "
+testPropertyTwoNoRecResults () |> printTestResultsNoRec
